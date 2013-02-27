1c96bfc805a4c1bc7f929c3709d0875572e7c953
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-02-27 19:20:04 +0000

    New version of arduino code better compartmentalizes sbus processing.

---
+++ Arduino/nostromo_v2/tmp102.h
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include "configuration.h"
+
+#include <Wire.h>
+
+#ifndef __TMP102_H
+#define __TMP102_H
+
+class TMP102 {
+public:
+  TMP102() {}
+  
+  float getTemp() { return temp; }
+  void updateReading();
+  
+private:
+  float temp;
+  
+  const static int sensorAddress = (0x91>>1);
+};
+
+#endif
+
