32debddeb2dba9df4776c9412cba82ec3c137c6e
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-04-12 23:17:05 +0000

    slightly modified nostromo_v2 arduino code for new kayak

---
+++ Arduino/new_kayak/ACS715.h
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+#include "configuration.h"
+
+class ACS715 {
+public:
+  int getMilliAmps();
+private:
+  static const int pin = 14;
+
+};
---
+++ Arduino/new_kayak/gumstix_serial.h
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "configuration.h"
+
+#ifndef __GUMSTIX_SERIAL_H
+#define __GUMSTIX_SERIAL_H
+
+class GUMSTIX_SERIAL {
+public:
+  GUMSTIX_SERIAL(Stream& port);
+  void doWork();
+  void initialize();
+  
+  int getRudderCommand() { return rudder; }
+  int getThrustCommand() { return thrust; }
+  unsigned long getLastCommandTime() { return last_command_time; }
+  
+private:
+  Stream& _port;
+  static const int shortestCompletePacket = 20;
+  
+  int thrust, rudder;
+  unsigned long last_command_time;
+  
+  char buffer[100];
+  int buffer_index;
+  
+  void readBuffer();
+  int processBuffer();
+  int findLine( int index );
+  void shiftBuffer( int shift );
+  
+  void parseMotorCommand(int index, int stopIndex);
+  void parseRadioCommand(int index, int stopIndex);
+};
+
+#endif
+
+
