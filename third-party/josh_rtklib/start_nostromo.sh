193842832228726768fd1808638cc0a123ba2789
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-06-18 17:20:18 +0000

    fixed command file

--- third-party/josh_rtklib/start_nostromo.sh
+++ third-party/josh_rtklib/start_nostromo.sh
@@ -1,1 +1,1 @@
-../rtklib/app/str2str/gcc/str2str -in serial://ttyACM0 -out tcpsvr://:50000 -c ublox_base.cmd
+../rtklib/app/str2str/gcc/str2str -in serial://ttyACM0 -out tcpsvr://:50000 -c ublox_vehicle.cmd
