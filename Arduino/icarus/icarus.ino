d6b5b074b0cb5b3d318ca85bed6d57141ec7e367
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-03-11 00:52:29 +0000

    icarus arduino code in development

--- Arduino/icarus/icarus.ino
+++ Arduino/icarus/icarus.ino
@@ -1,17 +1,28 @@
+#include "Arduino.h"
+#include "Wire.h"
 
-static const float Rhigh = 6;
-static const float Rlow = 3.14;
+#include "tmp102.h"
+
+static const float divider = 5.54;
+static const float fudge = 0.1;
+
+TMP102 tmp102 = TMP102();
 
 void setup() {  
   pinMode(A0, INPUT);
   
-  Serial1.begin(9600);
+  Wire.begin();
+  
+  Serial.begin(9600);
 }
 
 void loop() {
   delay(500);
   int val = analogRead(A0);
-  float pinvoltage = val / 3.3;
-  float batteryvoltage = pinvoltage * (Rhigh + Rlow) / Rlow;
-  Serial1.println(batteryvoltage);
+  float pinvoltage = val * 5.0 / 1024;
+  float batteryvoltage = pinvoltage * divider + 0.1;
+  tmp102.updateReading();
+  Serial.print(tmp102.getTemp());
+  Serial.print("   ");
+  Serial.println(batteryvoltage);
 }
---
+++ Arduino/icarus/tmp102.h
@@ -0,0 +1,37 @@
+#include "Arduino.h"
+
+#include <Wire.h>
+
+#ifndef __TMP102_H
+#define __TMP102_H
+
+class TMP102 {
+public:
+  TMP102() {}
+  
+  float getTemp() { return temp; }
+  void updateReading() {
+    // step 1: request reading from sensor 
+    Wire.requestFrom(sensorAddress,2); 
+    if (2 <= Wire.available())  // if two bytes were received 
+    {
+      int temperature;
+      byte msb, lsb;
+      
+      msb = Wire.read();  // receive high byte (full degrees)
+      lsb = Wire.read();  // receive low byte (fraction degrees) 
+      temperature = ((msb) << 4);  // MSB
+      temperature |= (lsb >> 4);   // LSB
+      
+      temp = temperature*0.0625;
+    }
+  }
+  
+private:
+  float temp;
+  
+  const static int sensorAddress = (0x91>>1);
+};
+
+#endif
+
