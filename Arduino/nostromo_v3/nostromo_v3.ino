8464033df189c0c74f93781055ecc9c87f474142
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-04-03 23:01:53 +0000

    new nostromo code

---
+++ Arduino/nostromo_v3/nostromo_v3.ino
@@ -0,0 +1,133 @@
+// pin configuration
+#define RelayPinVbatt 56
+#define RelayPin12V 55
+
+// thrust limiting when turning
+#define limitStartAngle 30 // angle past which limiting should start
+#define limitOffset 0 // ammount shich should be limited at start angle
+#define limitSlope 10 // additional thrust limited per degree past start angle
+
+#include <Wire.h>
+#include <Servo.h>
+#include "interface.h"
+#include "roboteq.h"
+#include "sbus.h"
+#include "radio.h"
+#include "azimuth.h"
+#include "tmp102.h"
+#include "battery.h"
+#include "gumstix.h"
+#include "lights.h"
+
+SBUS sbus = SBUS(Serial3);
+Interface interface = Interface(Serial);
+Roboteq roboteq = Roboteq(Serial2);
+RadioControl radio = RadioControl();
+Azimuth azimuth = Azimuth();
+TMP102 tmp102 = TMP102();
+Battery battery = Battery();
+GumstixSerial gumstix = GumstixSerial(Serial1);
+Lights lights = Lights();
+
+void setup() {
+  Wire.begin();
+  
+  // turn on 12v regulator and Vbatt relay
+  pinMode(RelayPinVbatt, OUTPUT);
+  digitalWrite(RelayPinVbatt, HIGH);
+  pinMode(RelayPin12V, OUTPUT);
+  digitalWrite(RelayPin12V, HIGH);
+  
+  // initialize and set defaults as needed
+  radio.initialize();
+  azimuth.initialize();
+  radio.powerBullet();
+    
+  // interface serial
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  
+  // other serials
+  Serial3.begin(100000);
+  Serial1.begin(115200);
+  Serial2.begin(115200);
+  delay(500);
+  
+  roboteq.setLights(false);
+}
+
+// timing
+int iteration = 0;
+float core_looprate;
+unsigned long command_timer_start = 0;
+
+// control
+int desired_thrust, desired_rudder;
+int thrust_limit = 1000;
+
+
+void loop() {
+  sbus.doWork();
+  interface.doWork();
+  roboteq.doWork();
+  battery.doWork();
+  gumstix.doWork();
+  lights.doWork();
+  
+  // inform azimuth of e-stop and thrust limit settings
+  azimuth.setEStop(roboteq.getStopSwitch());
+  azimuth.setThrustLimit(battery.getThrustLimit());
+  lights.setEStop(roboteq.getStopSwitch());
+  
+  // go through potential command sources
+  if (interface.getManualEnabled()) { // get commands from manual input
+    desired_thrust = interface.getThrustCommand();
+    desired_rudder = interface.getRudderCommand();
+    interface.setCommandSource(s_manual);
+  } else if (sbus.getRcAvailable() && !sbus.getSerialEnable()) { // get commands from rc
+    interface.setCommandSource(s_rc);
+    desired_rudder = sbus.getRudder();
+    if (sbus.getThrustEnable())
+      desired_thrust = sbus.getThrust();
+    else
+      desired_thrust = 0;
+  } else if (gumstix.getCommandsAvailable()) { // get commands from gumstix
+    desired_thrust = gumstix.getThrustCommand();
+    desired_rudder = gumstix.getRudderCommand();
+    interface.setCommandSource(s_gumstix);
+  } else { // do nothing
+    desired_thrust = 0;
+    desired_rudder = 0;
+    interface.setCommandSource(s_no_input);
+  }
+  
+  // send rudder command
+  azimuth.setAngle(desired_rudder);
+  
+  // apply battery thrust limiting
+  thrust_limit = battery.getThrustLimit();
+  desired_thrust = constrain(desired_thrust, -thrust_limit, thrust_limit);
+  
+  // apply turn thrust limiting
+  int current_angle = azimuth.getCurrentAngle();
+  if ( abs(current_angle) >= limitStartAngle ) {
+    int turn_limit = (abs(current_angle)-limitStartAngle)*limitSlope + limitOffset;
+    desired_thrust = constrain( desired_thrust, -1000+turn_limit, 1000-turn_limit );
+  }
+  
+  // send thrust command
+  roboteq.setPower(desired_thrust);
+  
+  // timer
+  float command_timer_elapsed = millis() - command_timer_start;
+  if (command_timer_elapsed > 5000) {
+    roboteq.rolloverCounters(command_timer_elapsed/1000.0);
+    gumstix.rolloverCounters(command_timer_elapsed/1000.0);
+    core_looprate = iteration*1000.0/command_timer_elapsed;
+    tmp102.updateReading();
+    command_timer_start = millis();
+    iteration=0;
+  }
+  iteration++;
+  delay(1);
+}
---
+++ Arduino/nostromo_v3/sbus.h
@@ -0,0 +1,46 @@
+#ifndef SBUS_H
+#define SBUS_H
+
+#include "Arduino.h"
+
+#define SBUS_SIGNAL_OK          0x00
+#define SBUS_SIGNAL_LOST        0x01
+#define SBUS_SIGNAL_FAILSAFE    0x03
+
+class SBUS {
+public:
+  SBUS(Stream& port);
+int16_t channels[18];
+uint8_t  failsafe_status;
+   
+  void doWork();
+  
+  int getThrust();
+  int getRudder();
+  boolean getRcAvailable() { return use_rc; }
+  boolean getThrustEnable() { return thrust_enable; }
+  boolean getSerialEnable() { return serial_enable; }
+  
+private:
+  boolean use_rc, thrust_enable, serial_enable;
+  boolean iterate();
+
+Stream& _port;
+
+uint8_t sbus_data[25];
+int sbus_passthrough;
+uint8_t byte_in_sbus;
+uint8_t bit_in_sbus;
+uint8_t ch;
+uint8_t bit_in_channel;
+uint8_t bit_in_servo;
+uint8_t inBuffer[25];
+int bufferIndex;
+uint8_t inData;
+int toChannels;
+
+  void update_channels();
+  void feedLine();
+};
+
+#endif
