93683f7a9cbdd8ec548e24f5e41014866119ae46
Josh Leighton <leighton.josh@gmail.com>
2015-04-08 13:07:01 -0400

    fixing scripts for new directory structures

--- third-party/josh_rtklib/start_icarus.sh
+++ third-party/josh_rtklib/start_icarus.sh
@@ -1,1 +1,1 @@
-/home/josh/third-party/rtklib/app/str2str/gcc/str2str -in serial://ttyACM0 -out tcpsvr://:50000 -c /home/josh/third-party/josh_rtklib/ublox_vehicle.cmd
+/home/mit/hoverland/hover-kayak/third-party/rtklib/app/str2str/gcc/str2str -in serial://ttyACM0 -out tcpsvr://:50000 -c /home/mit/hoverland/hover-kayak/third-party/josh_rtklib/ublox_vehicle.cmd
