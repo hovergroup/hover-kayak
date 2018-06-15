34c293ccbeeac9935551ed7e064acdba31fb9407
Mei <mc2922@mit.edu>
2018-06-14 22:38:03 -0400

    build update

--- code/core/build_proto.sh
+++ code/core/build_proto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=src/protobuf --cpp_out=src/protobuf --proto_path=$HOME/hover-kayak/third-party/goby/include --proto_path=/usr/include src/protobuf/*.proto
+protoc --proto_path=src/protobuf --cpp_out=src/protobuf --proto_path=$HOME/goby/include --proto_path=/usr/include src/protobuf/*.proto
